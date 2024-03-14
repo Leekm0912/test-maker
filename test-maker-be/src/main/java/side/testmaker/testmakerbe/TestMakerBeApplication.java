56d0500c4182b456f23d91e674ccb413d41c657d
Lee Kyung-min <42348176+Leekm0912@users.noreply.github.com>
2024-03-14 15:28:25 +0900

    first commit

---
+++ test-maker-be/src/main/java/side/testmaker/testmakerbe/TestMakerBeApplication.java
@@ -0,0 +1,13 @@
+package side.testmaker.testmakerbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMakerBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMakerBeApplication.class, args);
+    }
+
+}
---
+++ test-maker-be/src/test/java/side/testmaker/testmakerbe/TestMakerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package side.testmaker.testmakerbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMakerBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
